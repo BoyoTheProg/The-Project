a17c15401215da963a0bb87aff2e93bfa9871f29
BoyoTheProg <136482603+BoyoTheProg@users.noreply.github.com>
2024-03-24 22:54:46 +0200

    Updated interface

--- src/main/java/com/movieapp/controller/GenreController.java
+++ src/main/java/com/movieapp/controller/GenreController.java
@@ -10,7 +10,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class GenreController {
@@ -28,6 +31,7 @@ public class GenreController {
         // Add logic to fetch movies by genre and populate the model
         MovieHomeDto movies = movieService.getGenreViewData(genre);
         model.addAttribute("movies", movies.getAvailableMovies());
+        model.addAttribute("mostWatchedMovie", movieService.getMostWatchedMovieByGenre(genre.toString()));
 
         // Return the name of the Thymeleaf template for displaying genre details
         return "genre-details"; // Change this to the actual template name
@@ -37,9 +41,19 @@ public class GenreController {
 
 
     @GetMapping("/genres")
-    public String genresView( Model model) {
+    public String genresView(Model model) {
         GenreType[] allGenres = GenreType.values();
+        List<String> posterUrls = new ArrayList<>();
+
+        for (GenreType genre : allGenres) {
+            String posterUrl = movieService.getRandomMoviePoster(genre);
+            posterUrls.add(posterUrl);
+        }
+
+        model.addAttribute("posterUrls", posterUrls);
         model.addAttribute("genres", allGenres);
         return "genres";
     }
+
+
 }
